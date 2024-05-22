3fb35b6d996cdf271f978c9b53e45426b27074ab
gaeljarma <gaeljarma21@gmail.com>
2024-05-22 11:28:12 -0300

    pruebas

--- App.tsx
+++ App.tsx
@@ -4,13 +4,15 @@ import { StyleSheet, Text, View, Alert, TextInput } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.titulo}>Login</Text>
-      <Text style={styles.titulo}>Sign In</Text>
+      <Text style={styles.titulo}>Hello!</Text>
+      <Text style={styles.Subtitulo}>Sign In</Text>
       <TextInput
         placeholder = "Email"
+        style={styles.textInput}
       />
       <TextInput
-        placeholder = "password"
+        placeholder = "Password"
+        style={styles.textInput}
       />
       <StatusBar style="auto" />
     </View>
@@ -20,12 +22,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 10,
-    backgroundColor: 'purple',
+    backgroundColor: '#f1f1f1',
     alignItems: 'center',
     justifyContent: 'center',
   },
   titulo:{
     fontSize: 50,
-    fontColor: 'white',
-  }
+    color: '#000',
+    fontWeight: 'bold',
+  },
+  Subtitulo:{
+    fontSize: 20,
+    color: 'gray',
+  },
+  textInput:{
+    padding: 10,
+    paddingStart: 30,
+    height: 50,
+    width: '80%',
+    marginTop: 20,
+    borderRadius: 30,
+    backgroundColor: '#fff'
+  },
 });
