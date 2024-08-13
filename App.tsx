70ae8bc84dbbee7250772c3d9c89868a95904dff
Catalinamk <47573129@est.ort.edu.ar>
2024-08-13 09:43:50 -0300

    guarde algo viejo

--- App.tsx
+++ App.tsx
@@ -4,11 +4,11 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { BlurView } from 'expo-blur';
 import { Calendar, CalendarList, Agenda, LocaleConfig } from 'react-native-calendars';
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { createClient } from "@supabase/supabase-js";
 
 const Stack = createNativeStackNavigator();
-
+const supabase = createClient("https://yteasuzcyusqvmxsvggt.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0ZWFzdXpjeXVzcXZteHN2Z2d0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY5MjAxMzQsImV4cCI6MjAzMjQ5NjEzNH0.jWQhBPEmH3brwrnIOk0T-wBGO7JorH6A_wMVFtWLDd8");
 
 export default function App() {
   
