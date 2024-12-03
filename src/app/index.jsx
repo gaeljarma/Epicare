13fa1aed9850e09d686c37535e5e5533d2ad7b18
facujarma <facundojarma@gmail.com>
2024-12-03 15:59:43 -0300

    Agregar Perfil

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import LogInPage from "../components/LogInPage";
 import HomePage from "../components/HomePage";
 function index() {
-  return <HomePage />;
+  return <LogInPage />;
 }
 
 export default index;
