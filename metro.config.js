3db4cde7c3581e126952f7e079d5b941a0cd8abe
Catalinamk <47573129@est.ort.edu.ar>
2024-12-09 21:22:31 -0300

    Calendario, contactos emer y request ia

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,8 @@
-const {getDefaultConfig} = require('expo/metro-config');
-const config = getDefaultConfig(__dirname)
-config.resolver.assetExts.push('bin')
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+config.resolver.assetExts.push('bin');
+config.resolver.sourceExts.push('json');
+
 module.exports = config;
